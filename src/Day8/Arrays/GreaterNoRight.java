051f0027c25aee0fe20ca474c1a6a6ea0793890a
Ananthabarani <147480681+Ananthabarani@users.noreply.github.com>
2025-01-06 15:55:34 +0530

    Update GreaterNoRight.java
    Day8

--- src/Day8/Arrays/GreaterNoRight.java
+++ src/Day8/Arrays/GreaterNoRight.java
@@ -1,3 +1,9 @@
+//For each element in an array, find the next greater element to its right.
+// If no such element exists, return -1 for that number.
+//Example:
+//Input: nums = [4, 5, 2, 10]
+//Output: [5, 10, 10, -1]
+//Explanation: The next greater element for 4 is 5, for 5 is 10, for 2 is 10, and for 10 there is no greater element.
 package Day8.Arrays;
 
 import java.util.Scanner;
